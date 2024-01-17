2dfceca9b2387ccab8fc6ab249cbbddfdcc5fd84
MLoth <MLoth@users.noreply.github.com>
2024-01-17 13:49:02 +0100

    feat: start files

---
+++ main.js
@@ -0,0 +1,42 @@
+import './style.css'
+
+import Particle from './script/Particle.js'
+
+new Particle(100, 100, 30, 'blue', { x: 1, y: 1 })
+
+const generateParticles = (amount = 100) => {
+  const particles = []
+  for (let i = 0; i < amount; i++) {
+    particles.push(
+      new Particle(
+        Math.random() * window.innerWidth,
+        Math.random() * window.innerHeight,
+        Math.random() * 5,
+        'blue',
+        {
+          x: (Math.random() - 0.5) * 5,
+          y: (Math.random() - 0.5) * 5,
+        },
+      ),
+    )
+  }
+  return particles
+}
+
+const particles = generateParticles(1000)
+
+const canvas = document.querySelector('canvas')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const ctx = canvas.getContext('2d')
+
+const animate = () => {
+  requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  particles.forEach((particle) => {
+    particle.update(ctx)
+  })
+}
+
+animate()
---
+++ script/Particle.js
@@ -0,0 +1,39 @@
+export default class Particle {
+  friction = 0.99
+
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+    this.alpha = 1
+  }
+
+  draw(ctx) {
+    ctx.globalAlpha = this.alpha
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+    ctx.restore()
+  }
+
+  update(ctx) {
+    this.draw(ctx)
+    // this.velocity.x *= this.friction
+    // this.velocity.y *= this.friction
+
+    if (this.x + this.radius > window.innerWidth || this.x - this.radius < 0) {
+      this.velocity.x = -this.velocity.x
+    }
+
+    if (this.y + this.radius > window.innerHeight || this.y - this.radius < 0) {
+      this.velocity.y = -this.velocity.y
+    }
+
+    this.x += this.velocity.x
+    this.y += this.velocity.y
+    // this.alpha -= 0.01
+  }
+}
